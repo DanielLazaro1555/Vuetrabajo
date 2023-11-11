7da1947f11563d5c076e33a25ddc9801b77e611d
DanielLazaro1555 <danielsam_m@hotmail.com>
2023-11-10 22:43:34 -0500

    Se agrega el proyecto

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,160 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="country-grid">
+    <div v-for="country in countries" :key="country.code" class="country-card">
+      <div class="country-info">
+        <div class="image-container">
+          <img :src="'/imagen.jpg'" alt="Country Image" class="country-image" />
+        </div>
+        <div class="text-container">
+          <div class="flag-container">
+            <img :src="getFlagUrl(country.code)" alt="Flag" class="flag" />
+          </div>
+          <div class="text-content">
+            <h2>{{ country.name }}</h2>
+            <p class="continent-name">{{ country.continent.name }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  mounted() {
+    this.fetchCountries();
+  },
+  methods: {
+    async fetchCountries() {
+      try {
+        const response = await fetch('https://countries.trevorblades.com/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: `
+              {
+                countries {
+                  code
+                  name
+                  continent {
+                    name
+                  }
+                }
+              }
+            `,
+          }),
+        });
+
+        const { data } = await response.json();
+        this.countries = data.countries;
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    },
+    getFlagUrl(countryCode) {
+      // Utilizando flagcdn.com
+      return `https://flagcdn.com/w20/${countryCode.toLowerCase()}.png`;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+#app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* Ajusta la altura según tus necesidades */
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+/* Agrega estilos según sea necesario */
+.country-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.country-card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  overflow: hidden;
+  position: relative;
+  max-width: 70%;
+  margin: 0 auto; /* Centra la tarjeta en el contenedor */
 }
-a {
-  color: #42b983;
+
+
+
+.country-info {
+  margin-top: 10px;
 }
+
+.continent-name {
+  color: #555; /* Color gris para el nombre del continente */
+  margin-top: 5px; /* Espacio entre el nombre del país y el nombre del continente */
+}
+
+.image-container {
+  position: relative;
+}
+
+.flag-container {
+  margin-left: 0; /* Elimina el margen izquierdo */
+  margin-right: 5px; /* Espacio entre la bandera y el texto */
+  align-self: center; /* Centra verticalmente el .flag-container */
+}
+
+.image-overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px; /* Altura del fondo blanco en la parte inferior */
+  background-color: white; /* Color del fondo blanco */
+  z-index: 1; /* Asegura que el fondo blanco esté sobre la imagen */
+}
+
+.flag {
+  width: 70px; /* Ancho más grande de las banderas */
+  height: 60px; /* Altura más grande de las banderas */
+  align-self: center; /* Alinea la bandera en el centro vertical del contenedor */
+  z-index: 2; /* Asegura que la bandera esté sobre el fondo blanco */
+}
+
+.country-image {
+  width: 100%; /* Ocupa todo el ancho del contenedor */
+  height: auto; /* Permite que la altura se ajuste automáticamente según el ancho */
+  margin-top: -10px; /* Ajusta el margen superior para superponer la imagen sobre el contenido */
+  z-index: 0; /* Asegura que la imagen esté detrás del fondo blanco y la bandera */
+}
+
+.text-container {
+  display: flex;
+  font-size: 10px;
+  align-items: flex-start;
+  position: relative;
+  z-index: 2; /* Asegura que el contenido de texto esté sobre la imagen y el fondo blanco */
+  background-color: white; /* Fondo blanco */
+  padding: 10px; /* Espaciado interno para mejorar la legibilidad */
+  border-radius: 0 0 10px 10px; /* Ajusta el radio de borde para que coincida con la esquina de la imagen */
+}
+
+.text-content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+
+/* ... otros estilos ... */
 </style>
